815e840ffee329d7f4276b05fbe59065f94f4c9a
Tiago <ztiagok@gmail.com>
2022-08-01 20:03:11 -0500

    c6 - 5º Requisito Postergado/6º e 7º Requisito Completo

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 export default class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            <th> Descrição </th>
+            <th> Tag </th>
+            <th> Método de pagamento </th>
+            <th> Valor </th>
+            <th> Moeda </th>
+            <th> Câmbio utilizado </th>
+            <th> Valor convertido </th>
+            <th> Moeda de conversão </th>
+            <th> Editar/Excluir </th>
+          </tr>
+        </thead>
+      </table>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Wallet extends React.Component {
       <>
         <Header />
         <WalletForm />
+        <Table />
       </>
     );
   }
